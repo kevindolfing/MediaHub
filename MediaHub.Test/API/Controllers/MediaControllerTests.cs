c7debb504127c04d354cd51b9aa4465e8f39e1ef
kevin <65958288+KevinDaGame@users.noreply.github.com>
2024-03-10 17:22:21 +0100

    Fix tests

---
+++ MediaHub.Test/API/Controllers/MediaControllerTests.cs
@@ -0,0 +1,32 @@
+using MediaHub.API.Controllers;
+using MediaHub.DAL.FS.Model;
+using MediaHub.DAL.FS.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace MediaHub.Test.API.Controllers;
+
+[TestClass]
+public class MediaControllerTests
+{
+    [TestMethod]
+    public void GetMedia_ReturnsMedia_WhenServiceReturnsMedia()
+    {
+        // Arrange
+        var media = new List<IMedia>
+        {
+            new MediaFolder { Name = "folder1" },
+            new Media { Name = "file1.txt" },
+            new Media { Name = "file2.txt" }
+        };
+        var mockService = new Mock<IMediaService>();
+        mockService.Setup(service => service.GetMedia()).Returns(media);
+        var controller = new MediaController(new Mock<ILogger<MediaController>>().Object, mockService.Object);
+
+        // Act
+        IEnumerable<IMedia> result = controller.GetMedia();
+
+        // Assert
+        Assert.AreEqual(media, result);
+    }
+}
